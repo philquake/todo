8b236be9daaff04bca188be13f46285832fdbecf
philquake <phillipcole123@gmail.com>
2022-04-01 13:45:04 -0500

    Finalize basic todo list features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import logo from './logo.svg';
-import react from "react";
+import React from "react";
 import './App.css';
-import {Button, Card, Form } from 'react-bootstrap';import 'boostrap/dist/css/bootstrap.min.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      >
+        <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+      <div>
+      <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+      <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+  )
+}
+
+function FormTodo ({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit = {handleSubmit}>
+      <Form.Group>
+        <Form.Label><b>Add Todo</b></Form.Label>
+        <Form.Control type ="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder = "Add new todo" />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">Submit</Button>
+      </Form>
+  );
+}
 
 function App() {
   const [todos, setTodos] = React.useState ([
@@ -14,19 +49,20 @@ function App() {
   const addTodo = text => {
     const newTodos = [...todos, {text}];
     setTodos(newTodos);
-  }
+  };
 
   const markTodo = index => {
     const newTodos = [...todos,];
     newTodos[index].isDone = true;
     setTodos(newTodos);
-  }
+  };
 
   const removeTodo = index => {
-    const newTodos = [...todos,];
-    newTodos[index].splice(index, 1);
+    const newTodos = [...todos];
+    // newTodos[index].splice(index, 1);
+    delete newTodos[index]
     setTodos(newTodos);
-  }
+  };
 
   return (
     <div className="app">
